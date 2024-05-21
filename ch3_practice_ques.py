ec3a9dd9037caf13c2568ae166f3837592d56421
Codes Priyansh <codespriyansh@gmail.com>
2024-05-22 03:42:42 +0530

    Add files via upload

---
+++ ch3_practice_ques.py
@@ -0,0 +1,38 @@
+#solutions for ch-3 functions
+
+#q1 functions are great for organizing the code.
+
+#q2
+# def example():
+    #print("hello.")
+    
+#example()
+
+#q3 def 
+
+#q4A function consists of the def statement and the code in its def clause.
+#A function call is what moves the program execution into the function,
+#and the function call evaluates to the function’s return value.
+
+#q5 There is one global scope, and a local scope is created whenever a function is called.
+
+#q6 When a function returns, the local scope is destroyed, and all the variables in it are forgotten
+
+#q7 A return value is the value that a function call evaluates to. Like any
+#value, a return value can be used as part of an expression
+
+#q8 None
+
+#q9 global hello
+   #hello 
+    
+#q10 Nonetype
+
+#q11 That import statement imports a module named areallyourpetsnamederic.
+#(This isn’t a real Python module, by the way.)
+
+#q12 spam.bacon() 
+
+#q13 by error handling i.e. using try and except
+
+#q14 code which might produce an error, except: output if an error is produced. 
