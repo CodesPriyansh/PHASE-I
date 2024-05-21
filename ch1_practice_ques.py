9eafbe468423f880a2fc2032db6ddfbc70f719c2
Codes Priyansh <codespriyansh@gmail.com>
2024-05-22 03:30:07 +0530

    Add files via upload

---
+++ ch1_practice_ques.py
@@ -0,0 +1,52 @@
+#ch 1 - practice questions
+
+#q1
+'''
+
+-,+,/,* are operators and -88.0, 5, 'hello' are values.
+
+'''
+
+#q2
+'''
+
+spam is a variable and 'spam' is a string.
+
+'''
+
+#q3
+'''
+
+integers, floating-point numbers and strings
+
+'''
+
+#q4
+'''
+an expression is a combination of values and operators, all expressions 
+evaluate to a single value
+
+'''
+
+#q5
+'''
+all expressions evaluate to a single value, a statement does not.
+
+''' 
+
+#q6 value of bacon will be 21
+
+#q7 
+# 'spam' + 'spamspam' = spamspamspam
+# 'spam' *3 = spamspamspam 
+
+#q8 a variable in python can't start with a number, instead of 100 it can be egg100, a100,etc.
+
+#q9 str(), int(), float() 
+
+#q10 because '+' operator can add both strings or both numbers, not one number and one string.
+# the problem can be solved this way: 'I have eaten ' + str(99) + ' burritos.'
+
+
+
+
---
+++ ch2_practice_ques.py
@@ -0,0 +1,82 @@
+#ch2 - solutions 
+
+#q1 True , False
+
+#q2 not, and, or
+
+#q3 
+'''
+False or False
+True or False
+False or True
+False and False
+False and True
+True and False
+not False
+not True
+'''
+
+#q4 
+'''
+1. F
+2. F
+3. T
+4. F
+5. F
+6. T
+'''
+
+#q5 ==, !=, <, >, <=, >=
+
+#q6 equal to operator(==) and assignment(=)
+# a == 5 means a is equal to the value of 5.
+#a = 5 means a holds the value of 5, it's not equal to it. 
+
+#q7 A condition is an expression used in a flow control statement that evaluates to a Boolean value
+
+#q8
+'''
+print('eggs')
+if spam > 5:
+    print('bacon')
+else:
+    print('ham')
+print('spam')
+'''
+
+#q9 
+'''
+spam = input()
+if spam == 1:
+    print('hello')
+elif spam == 2:
+    print('Howdy')
+else:
+    print('Greetings!')
+'''
+
+#q10 Ctrl + C
+
+#q11 'break' exists the loop, 'continue' goes back to the start of the loop.
+
+#q12 
+# range(10) it ranges from 0 to 9
+# range(0, 10) it ranges from 0 to 9, where 0 is the start value and 9 is the stop value.
+# range(0, 10, 1) it ranges from 0 to 9 and increase the variable by 1 number everytime.
+
+#q13 
+'''
+uses a for loop
+
+for i in range(1, 11):
+    print(i)
+    
+uses a while loop
+
+i = 1
+while i < 11:
+    print(i)
+    i += 1
+'''
+
+#q14 spam.bacon() 
